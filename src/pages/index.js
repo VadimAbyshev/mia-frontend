15e3f8cb269f1cfb0dba3c68cf17fb5d651e1a5f
Vadim <boss.abyshev@gmail.com>
2023-11-20 17:05:03 +0500

    2 str

---
+++ src/pages/index.js
@@ -0,0 +1,92 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  direction: 'vertical', 
+  slidesPerView: 1, 
+  spaceBetween: 32,
+  mousewheel: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {// настройки для разных разрешений
+    768: {
+        slidesPerView: 'auto',
+        spaceBetween: 8
+    },
+    1050: {
+        slidesPerView: 1,
+        spaceBetween: 8
+    },
+    1060: {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        slidesPerView: 'auto',
+    },
+    1700: {
+      slidesPerView: 'auto',
+      spaceBetween: 150,
+      slidesPerView: 'auto',
+  }
+},
+// autoplay: {
+//   delay: 1000,
+// },
+
+});
+
+
+
+const accordion = document.querySelector('.accordion');
+const accordionOpenButton = document.querySelectorAll('.accordion-button');
+
+const burgerButton = document.querySelector('.header__burger')
+
+const  buergerMenu = document.querySelector('.header__nav')
+
+
+function openBurgerMenu(){
+  buergerMenu.classList.add('header__nav-open')
+}
+function closeBurgerMenu(){
+  buergerMenu.classList.remove('navigation_nav_open')
+}
+
+
+
+
+const accordionCatMobile = document.querySelector('.accordion-mobile');
+const accordionOpenTitleButton = document.querySelector('.categories__button-title');
+
+
+
+
+
+accordionOpenButton.forEach((elem)=>{
+  elem.addEventListener('click',()=>{
+ 
+      elem.parentNode.querySelector('.accordion').classList.toggle('accordion_opened')
+
+  })
+})
+
+
+
+  if (window.innerWidth > 450) {
+    accordionOpenTitleButton.disabled = true;
+  } else {
+    accordionOpenTitleButton.disabled = false;
+  }
+ 
+
+
+burgerButton.addEventListener('click', openBurgerMenu)
+
+
+const acc = document.querySelectorAll('.accordion');
+
+
+
